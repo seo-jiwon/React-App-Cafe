0f22a27bcf20ffa3853406b0fba4387c6359378f
seo-jiwon <sschong48@naver.com>
2022-11-24 15:07:32 +0900

    소스코드 정리

--- App.js
+++ App.js
@@ -1,114 +1,176 @@
 import * as React from 'react';
-import {useRef, useEffect} from 'react';
-import  {useState} from 'react';
-import { Text, View, StyleSheet, TextInput, Button, ScrollView, Alert ,Pressable, SafeAreaView} from 'react-native';
+import { useRef, useEffect, useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button, ScrollView, Alert, Pressable, SafeAreaView } from 'react-native';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
-
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function App() {
+  
   const [seatClickTable, setSeatClickTable] = React.useState([]); // 클릭한 좌석 정보를 테이블에 저장
   const [serverState, setServerState] = React.useState('Loading...'); // 서버 상태 메시지
   const [messageText, setMessageText] = React.useState(''); // 초기 TextInput 문자 상태
   const [disableButton, setDisableButton] = React.useState(true); // 초기 버튼 비활성화
-  const [inputFieldEmpty, setInputFieldEmpty] = React.useState(true); // TextInput 비었는지 상태
+  // const [inputFieldEmpty, setInputFieldEmpty] = React.useState(true); // TextInput 비었는지 상태
   const [serverMessages, setServerMessages] = React.useState([]); // 서버 메시지 리스트
-  const inputRef = React.useRef(null);
-  //var ws = React.useRef(new WebSocket('ws://10.32.15.49:8887')).current;
-  // useRef() Hook : current 속성을 가지고 있는 객체 반환 -> ??
+  const [userTable, setUserTable] = React.useState([]); // 서버에 접속한 사용자(이름) 저장
+  const [userTest, setUserTest] = React.useState([]); // 서버에 접속한 사용자(이름) 저장
+  const [isSelect, setSelect] = useState([false, false, false, false, false, false, false, false, false]); // 좌석 버튼 상태
 
-  //윤경
-  //const websocketUrl = 'ws://10.32.14.112:8080';
-  //지원
-  const websocketUrl = 'ws://10.32.12.158:8080';
-  //지원
-  //const websocketUrl = 'ws://127.0.0.1:8080';
-  //교수님
-  //const websocketUrl = 'ws://172.30.1.81:8080';
+  // Websocket URL
+  const websocketUrl = 'ws://192.168.35.85:8080';
 
-  let ws = React.useRef(null);  
-  const [isSelect, setSelect] = useState([false, false, false, false, false, false, false, false, false]);
   var name = '서지원';
   var data = {};
   var sendData = [];
 
+  // useRef() Hook : current 속성을 가지고 있는 객체 반환
+  let ws = useRef(null);
+
+  // useEffect() Hook : 컴포넌트가 렌더링 될 때마다 작업을 실행할 수 있도록 하는 Hook
+  // useEffect Hook 최초 1회 웹소켓 정의 -> 다른 함수에서 웹소켓 인스턴스 사용x -> useRef 사용
+  useEffect(() => {
+
+    if (!ws.current) {
+      ws.current = new WebSocket(websocketUrl);
+      const serverMessagesList = [];
+      const userList = [];
+
+      // 웹소켓 열리면 서버와 연결되고 비활성화 버튼 false 처리
+      ws.current.onopen = () => {
+        setServerState('연결성공 - 지원')
+        setDisableButton(false);
+        var data = {};
+        var sendData = [];
+        data['id'] = name;
+        data['req'] = 'con';
+        sendData.push(data);
+        var jsonData = JSON.stringify(sendData);
+        ws.current.send(jsonData);
+      };
+
+      // 웹소켓 닫히면 버튼 비활성화 true 처리
+      ws.current.onclose = (e) => {
+
+        setServerState('연결 종료 - 지원')
+        setDisableButton(true);
+      };
+
+      // 에러 발생 시 에러 메시지
+      ws.current.onerror = (e) => {
+        setServerState(e.message);
+      };
+
+      // 서버 메시지
+      ws.current.onmessage = (e) => {
+        // 서버 메시지 리스트에 텍스트 값(스크롤뷰에 작성된)을 넣음
+        serverMessagesList.push(e.data);
+        // console.log("서버메시지: " + e.data); // 서버메시지: [{"id":"서지원","req":"res","tnum":0}]
+        setServerMessages([...serverMessagesList]);
+        // console.log('message');
+        jsonRev = JSON.parse(e.data);
+
+        for (var i = 0; i < jsonRev.length; i++) {
+          // 예약한 경우
+          if (jsonRev[i].req == 'res') {
+            revSelect(jsonRev[i].tnum, true);
+            resTable(jsonRev[i]); // 아이디, 예약상태, 좌석번호 정보를 전달
+            serverMessages
+            // 취소한 경우
+          } else if (jsonRev[i].req == 'can') {
+            revSelect(jsonRev[i].tnum, false);
+            canTable(jsonRev[i]);
+
+            // 접속한 경우 - 접속 사용자 확인
+          } else if (jsonRev[i].req == 'con') {
+            // console.log("아이디는" + jsonRev[i].name);
+            userListTable(jsonRev[i]);
+          }
+        }
+      };
+    }
+
+  }, [])
+
+  // 좌석 버튼 생성 함수
   const getButton = (id) => {
-    console.log(ws.current);
-   
+    // console.log(ws.current);
+
     return (
       <Pressable
         style={[
           styles.buttonContainer,
-          {backgroundColor: isSelect[id] ? '#eeccff' : 'white'},
+          { backgroundColor: isSelect[id] ? '#eeccff' : 'white' },
         ]}
         onPress={() => {
-          console.log('onPress');
+          // console.log('onPress');
           // console.log(isSelect[0]);
 
-          //예약
-          if ( isSelect[id] == false ) {
+          // 예약
+          if (isSelect[id] == false) {
             Alert.alert(
-              id+1 + "번 좌석을 예약하시겠습니까?", // 타이틀
-              "",                             // 소제목
-              [                              // 버튼 배열
+              id + 1 + "번 좌석을 예약하시겠습니까?",   // 타이틀
+              "",                                // 소제목
+              [                                  // 버튼 배열
                 {
                   text: "아니요",                              // 버튼 제목
-                  onPress: () => console.log("예약 아니요"),     //onPress 이벤트시 콘솔창에 로그를 찍는다
+                  // onPress: () => console.log("예약 아니요"),    //onPress 이벤트시 콘솔창에 로그를 찍는다
                   style: "cancel"
                 },
-                { text: "네", onPress: () => 
-                  {
-                    if(ws.current !== null){
-                            // var data = {};
-                            // var sendData = [];
-                            data['id'] = name;
-                            if(isSelect[id] == false){
-                            data['req'] = 'res';
-                            }else{
-                              data['req'] = 'can';
-                            }
-                            data['tnum'] = id;
-                            sendData.push(data);
-                            var jsonData =JSON.stringify(sendData);
-                            ws.current.send(jsonData);
+                {
+                  text: "네", onPress: () => {
+                    if (ws.current !== null) {
+                      // var data = {};
+                      // var sendData = [];
+                      data['id'] = name;
+                      if (isSelect[id] == false) {
+                        data['req'] = 'res';
+                      } else {
+                        data['req'] = 'can';
+                      }
+                      data['tnum'] = id;
+                      sendData.push(data);
+                      var jsonData = JSON.stringify(sendData);
+                      ws.current.send(jsonData);
                     }
                   }
                 },
-                                                                      // 이벤트 발생시 로그를 찍는다
               ],
               { cancelable: false }
             );
-          } 
+          }
           // 예약 취소
           else {
             Alert.alert(
-              id+1 + "번 좌석을 예약 취소 하시겠습니까?", // 타이틀
-              "",                             // 소제목
-              [                              // 버튼 배열
+              id + 1 + "번 좌석을 예약 취소 하시겠습니까?", // 타이틀
+              "",                                   // 소제목
+              [                                     // 버튼 배열
                 {
-                  text: "아니요",                              // 버튼 제목
-                  onPress: () => console.log("예약 취소 아니요"),     //onPress 이벤트시 콘솔창에 로그를 찍는다
+                  text: "아니요",                                   // 버튼 제목
+                  // onPress: () => console.log("예약 취소 아니요"),     //onPress 이벤트시 콘솔창에 로그를 찍는다
                   style: "cancel"
                 },
-                { text: "네", onPress: () => 
-                  {
-                    if(ws.current !== null){
-                      for(var i=0; i<seatClickTable.length; i++) {
-                        if(seatClickTable[i].tnum == id) {
-                          if(seatClickTable[i].id == name) {
+                {
+                  text: "네", onPress: () => {
+                    if (ws.current !== null) {
+                      // 클릭한 좌석 정보를 테이블에 담은 길이만큼 돌 때
+                      for (var i = 0; i < seatClickTable.length; i++) {
+                        // 저장된 좌석번호 중 하나와 지금 클릭한 아이디(좌석번호)가 같을때
+                        if (seatClickTable[i].tnum == id) {
+                          // 저장된 아이디(사용자이름) 중 하나와 지금 클릭한 사용자이름이 같을때
+                          if (seatClickTable[i].id == name) {
                             var data = {};
                             var sendData = [];
                             data['id'] = name;
-                            if(isSelect[id] == false){
-                            data['req'] = 'res';
-                            }else{
+                            if (isSelect[id] == false) {
+                              data['req'] = 'res';
+                            } else {
                               data['req'] = 'can';
                             }
                             data['tnum'] = id;
                             sendData.push(data);
-                            var jsonData =JSON.stringify(sendData);
+                            var jsonData = JSON.stringify(sendData);
                             ws.current.send(jsonData);
-                            
+
                             break;
                           }
                           else {
@@ -125,134 +187,103 @@ export default function App() {
             );
           }
         }}>
-        <Text>{id+1}</Text>
+        <Text>{id + 1}</Text>
       </Pressable>
     );
   };
 
-  const revSelect = (id,flag) => {
-    isSelect[id] = flag; 
+  // 예약 및 예약취소 시 색상 변경 (좌석번호, 예약상태)를 받아옴)
+  const revSelect = (id, flag) => {
+    isSelect[id] = flag;
     setSelect([
-       isSelect[0],
-       isSelect[1],
-       isSelect[2],
-       isSelect[3],
-       isSelect[4],
-       isSelect[5],
-       isSelect[6],
-       isSelect[7],
-       isSelect[8],
-     ]);
+      isSelect[0],
+      isSelect[1],
+      isSelect[2],
+      isSelect[3],
+      isSelect[4],
+      isSelect[5],
+      isSelect[6],
+      isSelect[7],
+      isSelect[8],
+    ]);
   }
 
-  // 컴포넌트가 렌더링 될 때마다 작업을 실행할 수 있도록 하는 Hook
-  // useEffect Hook 최초 1회 웹소켓 정의 -> 다른 함수에서 웹소켓 인스턴스 사용x -> useRef 사용
-  React.useEffect(() => {
-   
-    if(!ws.current) {
-      ws.current = new WebSocket(websocketUrl);
-      const serverMessagesList = [];
-      // 웹소켓 열리면 서버와 연결되고 비활성화 버튼 false 처리
-      ws.current.onopen = () => {
-        setServerState('연결성공 - 지원')
-        setDisableButton(false);
-        var data = {};
-        var sendData = [];
-        data['id'] = name;
-        data['req'] = 'con';
-        sendData.push(data);
-        var jsonData =JSON.stringify(sendData);
-        ws.current.send(jsonData);
-
-      };
-      // 웹소켓 닫히면 버튼 비활성화 true 처리
-      ws.current.onclose = (e) => {
-      
-        setServerState('연결 종료 - 지원')
-        setDisableButton(true);
-      };
-      // 에러 발생 시 에러 메시지
-      ws.current.onerror = (e) => {
-        setServerState(e.message);
-      };
-      // 서버 메시지
-      ws.current.onmessage = (e) => {
-        // 서버 메시지 리스트에 텍스트 값(스크롤뷰에 작성된)을 넣음
-        serverMessagesList.push(e.data);
-        console.log("서버메시지: " + e.data);
-        setServerMessages([...serverMessagesList]);
-        console.log('message');
-        jsonRev = JSON.parse(e.data);
-
-        for(var i =0 ; i< jsonRev.length;i++){
-          if(jsonRev[i].req == 'res'){
-            revSelect(jsonRev[i].tnum,true);
-            resTable(jsonRev[i]); //3개정보
-          }else if(jsonRev[i].req == 'can'){
-            revSelect(jsonRev[i].tnum,false);
-            canTable(jsonRev[i]);
-          }
-        }
-
-
-
-       /* var testString =e.data;   // 원래 문자열
-        var regex = /[^0-9]/g;            // 숫자가 아닌 문자열을 선택하는 정규식
-        var result = testString.replace(regex, "");   */
-      };
-    }
-    
-  }, [])
-
+  // 예약 시 jsonRev로 3개 정보(사용자, 예약상태, 좌석번호)를 인자로 담아옴
+  // 예약 시 클릭한 좌석 정보를 seatClickTable 테이블 변수에 담아서 저장함.
   const resTable = (jsonRev) => {
-    tableInfo= {};
+    // server_json.js 파일에 예약 경우 부분과 동일(서버에서 req='res'인 경우 저장하는 부분)
+    tableInfo = {};
     tableInfo['tnum'] = jsonRev.tnum;
     tableInfo['id'] = jsonRev.id;
     tableInfo['req'] = jsonRev.req;
+
+    // 내가 만든 클릭한 좌석 정보를 담을 테이블 변수에 클릭한 사용자이름, 예약, 좌석 번호의 정보를 담는다
     seatClickTable.push(tableInfo);
   }
 
+  // 취소 시 jsonRev로 3개 정보(사용자, 예약상태, 좌석번호)를 인자로 담아옴
+  // 취소 시 클릭한 좌석 정보를 seatClickTable 테이블에서 제거한다.
   const canTable = (jsonRev) => {
-    for (var i=0; i<seatClickTable.length; i++) {
-      if(seatClickTable[i].tnum == jsonRev.tnum){
-        seatClickTable.splice(i,1);
+    // server_json.js 파일에 취소 경우 부분과 동일(서버에서 req='can'인 경우 저장하는 부분)
+    for (var i = 0; i < seatClickTable.length; i++) {
+      if (seatClickTable[i].tnum == jsonRev.tnum) {
+        seatClickTable.splice(i, 1);
         break;
       }
     }
   }
 
-  // 버튼 클릭 시 호출되는 함수
-  
+  const userListTable = (jsonRev) => {
+    userName = {};
+    userName['name'] = jsonRev.name;
+    userName['req'] = jsonRev.req;
+
+    userTest.push("" + userName.name);
+    // console.log("사용자리스트" + userTest);
+    setUserTest([...userTest]);
+
+  }
+
+  // 서버 메시지 전송 버튼을 클릭하는 경우 호출되는 함수
   const submitMessage = (e) => {
     ws.current.send(messageText);
-    console.log();    
-   // document.getElementById("b").color="red";
-   /* ws.current.send(messageText); // TextInput에 입력된 값(messageText)을 웹소켓에 전송
-    setMessageText('') // TextInput '' 로 변경
-    setInputFieldEmpty(true) // TextInput 비어도 된다*/
-  //  Alert.alert('좌석 예약이 완료되었습니다!');
-  
+    // console.log();
   }
+
   return (
     <View>
-      <View style={{
-        height: 70,
-        backgroundColor: '#7B08FF',
-        padding: 5,
-        justifyContent: "center",
-        shadowColor: "#000000",shadowOpacity: 0.3, shadowOffset: { width: 2, height: 2 }
-      }}>
-        <Text style={{ marginTop: 40, color: 'white'}}>{serverState}</Text>
+      {/* 서버 연결 상태를 알려주는 View */}
+      <View
+        style={{
+          height: 70, backgroundColor: '#7B08FF', padding: 5, justifyContent: "center",
+          shadowColor: "#000000", shadowOpacity: 0.3, shadowOffset: { width: 2, height: 2 }
+        }}
+      >
+        <Text style={{ marginTop: 40, color: 'white' }}>{serverState}</Text>
       </View>
-      <View style={{
-        margin: 5,
-        marginTop: 15,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: 'grey',
-        padding: 5,
-        height: 300,
-      }}>
+
+      {/* 서버에 접속한 사용자(이름)을 버튼으로 생성 */}
+      {/* <View style={{ flexDirection: 'row' }}>
+        <Text style={{ marginTop: 14, marginLeft: 10,fontSize: 17}}>접속자</Text> */}
+
+        {/* 접속한 사용자 이름 불러오는 곳 */}
+        {/* {
+          userTest.map((item, ind) => {
+            return (
+              <TouchableOpacity key={ind} style={{ marginTop: 7.5, marginLeft: 10, backgroundColor: "#eeceff", borderRadius: 5 }}>
+                <Text style={{ fontSize: 17, color: '#89008C', padding: 5 }}>{item}</Text>
+              </TouchableOpacity>
+            )
+          })}
+
+      </View> */}
+
+      {/* 서버 메시지를 나타내는 View */}
+      <View
+        style={{
+          margin: 5, marginTop: 5, borderRadius: 5, borderWidth: 1, borderColor: 'grey',
+          padding: 5, height: 300,
+        }}>
         <Text style={{ color: 'grey' }}>서버메시지</Text>
         <ScrollView>
           {
@@ -264,70 +295,50 @@ export default function App() {
           }
         </ScrollView>
       </View>
-      
-      <View style={{
-        flexDirection: 'row',
-        marginTop: 5
-      }}>
-        <TextInput style={{
-            borderWidth: 1,
-            borderRadius: 3,
-            borderColor: 'grey',
-            flexGrow: 1,
-            margin: 5,
-            padding: 5,
-            height: 50,
-            backgroundColor: 'white'
-          }} 
-          placeholder={'메시지를 작성하세요'} 
-          onChangeText={text => {
-            setMessageText(text)
-            setInputFieldEmpty(text.length > 0 ? false : true)  
-          }}
-          value={messageText}
-         />
-        <Button
-         onPress={submitMessage}
-         title={'Submit'} 
-         disabled={disableButton || inputFieldEmpty}
-        />
-      </View>
 
+      {/* 카페 View */}
       <View style={{
         flexGrow: 4,
       }}>
-        <View style={{ 
-          backgroundColor: "#eeceff", color: "white", height: 50, fontSize: 30, margin: 5,
-          justifyContent: 'center', borderWidth: 1, borderRadius: 20, borderColor:  "#eeceff",
-          shadowColor: "#89008C",shadowOpacity: 0.3, shadowOffset: { width: 2, height: 2 }  }}>
+
+        {/* 카페 타이틀 View */}
+        <View style={{
+          backgroundColor: "#eeceff", height: 50, fontSize: 30, margin: 5,
+          justifyContent: 'center', borderRadius: 20,
+          shadowColor: "#89008C", shadowOpacity: 0.3, shadowOffset: { width: 2, height: 2 }
+        }}>
           <Text style={{ margin: 5, color: "#89008C", fontSize: 30, fontWeight: "bold", paddingLeft: 5 }}>
             Cafe
           </Text>
         </View>
 
+        {/* 카페 좌석 View1 */}
         <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 25 }}>
-        {/* <View style={{ flexDirection: 'row', justifyContent: 'center' }}> */}
+
           {getButton(0)}
           {getButton(1)}
           {getButton(2)}
-          </View>
+        </View>
 
-          <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
-        {/* <View style={{ flexDirection: 'row', justifyContent: 'center' }}> */}
+        {/* 카페 좌석 View2 */}
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
           {getButton(3)}
           {getButton(4)}
           {getButton(5)}
-          </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
-        {/* <View style={{ flexDirection: 'row', justifyContent: 'center' }}> */}
+        </View>
+
+        {/* 카페 좌석 View3 */}
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
           {getButton(6)}
           {getButton(7)}
           {getButton(8)}
-          </View>
+        </View>
       </View>
     </View>
   );
 }
+
+// StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -335,7 +346,7 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     padding: 10,
   },
-    buttonContainer: {
+  buttonContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     width: 60,
@@ -346,7 +357,7 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     borderColor: 'white',
     shadowColor: "#000000",
-    shadowOpacity: 0.3, 
+    shadowOpacity: 0.3,
     shadowOffset: { width: 1, height: 1 },
   },
 });
--- server.js
+++ server.js
@@ -1,19 +1,86 @@
-//소켓 생성 npm install ws
+// 소켓 생성 npm install ws
 // 데몬으로 서버는 계속 기다리면서 포켓을 8080으로 설정
-var WebSocketServer = require('ws').Server, 
+var WebSocketServer = require('ws').Server; 
     wss = new WebSocketServer({port: 8080});
-    CLIENTS=[]; //접속자 serial을 담을 배열
+    var CLIENTS=[];         // 접속자 serial을 담을 배열
+    var tables = [];        // 테이블 정보를 담을 배열
+    var userTables = [];    // 사용자 정볼를 담을 배열
+   
 wss.on('connection', function(ws) {
-    CLIENTS.push(ws);
+   // CLIENTS.push(ws);
     ws.on('message', function(message) {
         console.log('received: %s', message); 
-        sendAll(message); //전부 보내줌, 자신에게도 -> 자신을 제외할려면 ws.sendAll
+        var jsonData = JSON.parse(message);
+        console.log(jsonData[0].id);       
+        console.log(jsonData[0].req);
+        
+        if(jsonData[0].req == 'con'){
+
+            // 사용자를 담을 userName 생성
+            userName = {};
+            userName['name'] = jsonData[0].id;
+            userName['req'] = jsonData[0].req;
+            userTables.push(userName);
+            sendAllExceptMe(message, ws);       // 자신을 제외한 사용자들에게 전송
+
+            ws.id = jsonData[0].id; 
+            CLIENTS.push(ws);
+
+            ws.send(JSON.stringify(userTables));
+            ws.send(JSON.stringify(tables));
+            sendAllExceptMe(message, ws);
+        }else if(jsonData[0].req == 'res'){
+            tableInfo= {};
+            tableInfo['tnum'] = jsonData[0].tnum;
+            tableInfo['id'] = jsonData[0].id;
+            tableInfo['req'] = jsonData[0].req;
+            tables.push(tableInfo);
+            console.log("res" + tables);
+            sendAll(message);                   // 자신을 포함한 모든 사용자에게 전송
+        }else if(jsonData[0].req == 'can'){
+            for (var i=0; i<tables.length; i++) {
+                if(tables[i].tnum == jsonData[0].tnum){
+                        tables.splice(i,1);
+                        break;
+                }
+            }
+            sendAll(message);
+        }else{
+            sendAll(message);
+        }
     });
-    ws.send("NEW USER JOINED");
+
+    ws.on('close', function(message) {
+        for (var i=0; i<CLIENTS.length; i++) {
+            if(CLIENTS[i].id == ws.id){
+              CLIENTS.splice(i,1);
+             break;
+            }
+       } 
+
+       for (var i=0; i<userTables.length; i++) {
+        if(userTables[i].id == ws.id){
+            userTables.splice(i,1);
+           break;
+          }
+       }
+    });
 });
-//클라이언트 배열만큼 돌면서 send
+
+// 자신을 포함한 모두에게 전송
 function sendAll (message) {
     for (var i=0; i<CLIENTS.length; i++) {
-        CLIENTS[i].send(''+ message);
+        console.log("클라이언트 " + CLIENTS[i].id); // userName
+        CLIENTS[i].send("" + message);
+    }
+}
+
+// 자신을 제외한 모두에게 전송
+function sendAllExceptMe (message, ws) {
+    for (var i=0; i<CLIENTS.length; i++) {
+        if(CLIENTS[i] != ws){
+            console.log("클라이언트 " + CLIENTS[i].id); // userName
+            CLIENTS[i].send("" + message);
+        }
     }
 }
